652be424a50fb129b5de873af1f5219aefadacec
hytiid <hytiid@gmail.com>
2020-08-01 11:11:24 +0900

    add: first commit

---
+++ CsvHelperWrapper/CsvHelperWrapper/CsvHelperWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Ionic.Zip;
+
+namespace CsvHelper
+{
+    public static class CsvHelperWrapper
+    {
+        public static CsvReader GenerateCsvReaderFromCsvContent(string zipPath, string csvPath, Encoding encoding)
+        {
+            using (ZipFile zip = new ZipFile(zipPath))
+                return GenerateCsvReaderFromCsvContent(zip, csvPath, encoding);
+        }
+        public static CsvReader GenerateCsvReaderFromCsvContent(ZipFile zip, string csvPath, Encoding encoding)
+        {
+            string content = ZipReader.ReadContent(zip, csvPath);
+            return GenerateCsvReaderFromCsvContent(content, encoding);
+        }
+        public static CsvReader GenerateCsvReaderFromCsvContent(string csvContent, Encoding encoding)
+        {
+            var ms = new MemoryStream(encoding.GetBytes(csvContent));
+            var sr = new StreamReader(ms);
+            return GenerateCsvReaderFromStream(sr);
+        }
+        public static CsvReader GenerateCsvReaderFromStream(StreamReader sr)
+        {
+            return new CsvReader(sr, System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        public static TData ReadContent<TData>(CsvReader csvReader, Func<CsvReader, TData> getRecord)
+        {
+            return ReadContents(csvReader, getRecord).First();
+        }
+        public static IEnumerable<TData> ReadContents<TData>(CsvReader csvReader, Func<CsvReader, TData> getRecord)
+        {
+            //read header
+            csvReader.Read();
+            csvReader.ReadHeader();
+            //read contents
+            var ret = new List<TData>();
+            while (csvReader.Read())
+                ret.Add(getRecord(csvReader));
+
+            return ret;
+        }
+    }
+}
---
+++ CsvHelperWrapper/CsvHelperWrapper/ZipReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Ionic.Zip;
+
+namespace CsvHelper
+{
+    public static class ZipReader
+    {
+        public static string ReadContent(string zipPath, string regex)
+        {
+            return ReadContent(ReadContents(zipPath, regex), regex);
+        }
+        public static string ReadContent(ZipFile zip, string regex)
+        {
+            return ReadContent(ReadContents(zip, regex), regex);
+        }
+
+        public static IEnumerable<string> ReadContents(string zipPath, string regex)
+        {
+            using (ZipFile zip = new ZipFile(zipPath))
+                return ReadContents(zip, regex);
+        }
+        public static IEnumerable<string> ReadContents(ZipFile zip, string regex)
+        {
+            Regex rgx = new Regex(regex);
+            IEnumerable<ZipEntry> entries = zip.Entries.Where(entry => rgx.IsMatch(entry.FileName));
+            return entries.Select(entry => ReadZipEntry(entry));
+        }
+
+        private static string ReadContent(IEnumerable<string> contents, string regex)
+        {
+            Require(contents != null,
+                string.Format("{0} is not included.", regex));
+            Require(contents.Count() == 1,
+                string.Format("found {0} file(s) for regex={1}. This method expect to find one file.", contents.Count(), regex));
+            return contents.First();
+        }
+        private static string ReadZipEntry(ZipEntry entry)
+        {
+            using (var sr = new StreamReader(entry.OpenReader()))
+                return sr.ReadToEnd();
+        }
+
+        private static void Require(bool expected, string errorMessage)
+        {
+            if (!expected)
+                throw new Exception(errorMessage);
+        }
+    }
+}
